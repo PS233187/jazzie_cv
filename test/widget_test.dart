f04e3a16315c81150b54500f3449e1929c6f3238
jaszy-md <jaszydh@gmail.com>
2024-06-16 16:48:51 +0200

    second_workflow

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,46 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:jazzies_cv/pages/home_page.dart';
 import 'package:jazzies_cv/pages/my_experience.dart';
-import 'package:lottie/lottie.dart';
+
 
 void main() {
-  testWidgets('MyExperiencePage UI Test', (WidgetTester tester) async {
-    // Build MyExperiencePage and trigger a frame.
-    await tester.pumpWidget(const MaterialApp(home: MyExperiencePage()));
+  testWidgets('HomePage UI Test', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: HomePage(),
+    ));
 
-    // Verify the AppBar title
-    expect(find.text('My Experience'), findsOneWidget);
+    // Verify the initial text 'Welkom!' is present.
+    expect(find.text('Welkom!'), findsOneWidget);
 
-    // Verify the Lottie animation is present
-    expect(find.byType(Lottie), findsOneWidget);
+    // Tap on one of the custom tiles (assuming we have 4 tiles).
+    await tester.tap(find.byType(GestureDetector).first);
+    await tester.pumpAndSettle(); // Wait for animation to complete.
 
-    // Verify the Skills header
-    expect(find.text('Skills'), findsOneWidget);
+    // Verify that the navigation happened correctly.
+    expect(find.byType(MyExperiencePage), findsOneWidget);
 
-    // Verify the presence of a specific skill
-    expect(find.text('Flutter'), findsOneWidget);
-    expect(find.text('Figma'), findsOneWidget);
-    expect(find.text('JavaScript'), findsOneWidget);
-    expect(find.text('WordPress'), findsOneWidget);
-
-    // Verify initial state of the popup explanation visibility
-    expect(find.text('Experience'), findsNothing);
-
-    // Tap the Lottie animation to show the popup explanation
-    await tester.tap(find.byType(Lottie));
-    await tester.pump();
-
-    // Verify the popup explanation is visible
-    expect(find.text('Experience'), findsOneWidget);
-
-    // Verify the close button is present in the popup
-    expect(find.byIcon(Icons.close), findsOneWidget);
-
-    // Tap the close button to hide the popup explanation
-    await tester.tap(find.byIcon(Icons.close));
-    await tester.pump();
-
-    // Verify the popup explanation is not visible anymore
-    expect(find.text('Experience'), findsNothing);
+    // You can add more tests as needed to validate other aspects of the UI.
   });
 }
